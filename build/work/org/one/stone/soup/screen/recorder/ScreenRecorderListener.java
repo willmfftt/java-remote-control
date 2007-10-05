bb90b6009773b143b001d5f7423b2ee8dfb4ff2b
nicholas.e.m.cross <nicholas.e.m.cross@7959d8fb-ae22-0410-9073-7bae18b39483>
2007-10-05 21:21:53 +0000

    Removed file/folder

--- build/work/org/one/stone/soup/screen/recorder/ScreenRecorderListener.java
+++
@@ -1,9 +0,0 @@
-package org.one.stone.soup.screen.recorder;
-
-import java.io.IOException;
-
-public interface ScreenRecorderListener {
-
-	public void frameRecorded(boolean fullFrame) throws IOException;
-	public void recordingStopped();
-}
