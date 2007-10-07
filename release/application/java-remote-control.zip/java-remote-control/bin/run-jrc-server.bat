97a8be40e409119134ff10b6fe33a57506f7181e
nicholas.e.m.cross <nicholas.e.m.cross@7959d8fb-ae22-0410-9073-7bae18b39483>
2007-10-07 21:38:35 +0000

    Changed package name to match code

--- release/application/java-remote-control.zip/java-remote-control/bin/run-jrc-server.bat
+++ release/application/java-remote-control.zip/java-remote-control/bin/run-jrc-server.bat
@@ -1,1 +1,1 @@
-start javaw -classpath ../lib/java-remote-control.jar;../lib/xapp.jar;../lib/open-forum.jar;../lib/core.jar org.one.stone.soup.xapp.application.jrc.XappJavaRemoteControlServer ../config/
+start javaw -classpath ../lib/java-remote-control.jar;../lib/xapp.jar;../lib/open-forum.jar;../lib/core.jar org.one.stone.soup.xapp.application.jrc.server.XappJavaRemoteControlServer ../config/
