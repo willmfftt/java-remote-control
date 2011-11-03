38642f2688f3ddd47be0bfe401b4532b77c108f0
nicholas.e.m.cross <nicholas.e.m.cross@7959d8fb-ae22-0410-9073-7bae18b39483>
2011-11-03 10:06:04 +0000

    Included reference screen grabber implementation

---
+++ src/main/java/com/wet/wired/jrc/frame/capture/FrameCaptureException.java
@@ -0,0 +1,13 @@
+package com.wet.wired.jrc.frame.capture;
+
+public class FrameCaptureException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1171595834676099399L;
+	
+	public FrameCaptureException(String message,Exception e) {
+		super(message,e);
+	}
+}
--- src/main/java/com/wet/wired/jrc/frame/capture/ScreenGrabber.java
+++ src/main/java/com/wet/wired/jrc/frame/capture/ScreenGrabber.java
@@ -1,21 +1,45 @@
 package com.wet.wired.jrc.frame.capture;
 
 import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import com.wet.wired.jrc.frame.Frame;
 
 public class ScreenGrabber implements FrameGrabber {
 
+	private Robot robot;
+	private Rectangle screenRectangle; 
+	
+	public ScreenGrabber() throws FrameCaptureException {
+		initialize();
+	}
+	
+	private void initialize() throws FrameCaptureException {
+		try{
+			robot = new Robot();
+			screenRectangle = new Rectangle(Toolkit.getDefaultToolkit ().getScreenSize());
+		} catch (Exception e) {
+			throw new FrameCaptureException("Failed to initialize ScreenGrabber",e);
+		}
+	}
+	
 	@Override
-	public Dimension getScreenSize() {
-		// TODO Auto-generated method stub
-		return null;
+	public Dimension getFrameSize() {
+		return screenRectangle.getSize();
 	}
 
 	@Override
 	public Frame grabFrame() {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedImage grab = robot.createScreenCapture(screenRectangle);
+		int[] rawData = new int[screenRectangle.width*screenRectangle.height];
+		grab.getRGB(0,0,screenRectangle.width,screenRectangle.height,rawData,0,screenRectangle.width);
+		
+		Frame frame = new Frame(screenRectangle.width,screenRectangle.height);
+		frame.setData(rawData);
+		return frame;
 	}
 
 }
